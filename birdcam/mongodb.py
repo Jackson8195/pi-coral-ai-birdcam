3c3cee2d615d78d9f8641a5eb6d03ca0d228deaf
Jackson8195 <jackson8@vt.edu>
2024-04-19 12:59:09 -0400

    Added environment variable for mongo pswd

--- birdcam/mongodb.py
+++ birdcam/mongodb.py
@@ -1,8 +1,16 @@
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://jackson8:<yourpasswordhere>@birdcam.tpshz91.mongodb.net/?retryWrites=true&w=majority&appName=BirdCam"
+
+# Get the password from the environment variable
+mongodb_password = os.environ.get("MONGODB_PASSWORD")
+
+# Construct the URI with the password
+uri = f"mongodb+srv://jackson8:{mongodb_password}@birdcam.tpshz91.mongodb.net/?retryWrites=true&w=majority&appName=BirdCam"
+
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
