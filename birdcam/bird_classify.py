019a764d63237b2d1730b8b450045d0bcdddc3f9
Jackson8195 <Jakeson8!117>
2024-04-28 19:58:39 -0400

    let it be done

--- birdcam/bird_classify.py
+++ birdcam/bird_classify.py
@@ -201,15 +201,21 @@ def main():
                         counter = Counter(hueVisitors)
                         # Get the most common element over the timer duration and its count
                         most_common_bird = counter.most_common(1)[0][0]
+                        print("type is: "type(most_common_bird))
+                        print(most_common_bird)
+                        '''
                         print("Most common: ",most_common_bird)
                         if any(most_common_bird == entry[0] for entry in hue_birds):
                             bird_lookup = [entry for entry in hue_birds if entry[0] == most_common_bird]
                             b.set_light('Countertop Lights', {'hue': bird_lookup[1], 'sat': bird_lookup[2], 'bri': bird_lookup[3]})
                         hueTimer = False
                         hueVisitors.append(visitor)
+                        '''
                     else:
+                        hueVisitors.append(visitor)
                         print("Hue Timer Running..")
                         pass
+                        
 
                     # If visit interval has past, clear visitors list
                     if timer:
